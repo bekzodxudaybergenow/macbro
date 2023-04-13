f9c05a7564272aafb31a12bc4fafa386f8e52a81
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2023-04-13 21:09:03 +0500

    remaining parts of the project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// humburgerBtn = document.querySelector('.humburger');
+// header = document.querySelector('.header');
+
+// humburgerBtn.addEventListener('click', () => {
+//     humburgerBtn.classList.toggle('active');
+//     header.classList.toggle('header_menu');
+// })
